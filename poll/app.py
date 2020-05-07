d3958ddf0d303f497a27eb7b816d909f290a384b
arthur baron <baronarthur3@gmail.com>
2020-05-07 17:42:10 +0000

    Resolve "node connect to postgres"

--- poll/app.py
+++ poll/app.py
@@ -16,7 +16,8 @@ app = Flask(__name__)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="127.0.0.1", db=0, socket_timeout=5)
+        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        #g.redis = Redis(host="127.0.0.1", db=0, socket_timeout=5)
     return g.redis
 
 
